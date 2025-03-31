26f0b7ff2c9b9e1272f707b39e721c804ea7c19a
Divo123 <topherharsh@gmail.com>
2025-03-31 14:07:00 +0700

    added video components

---
+++ client/src/Components/VideoMessageBox.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  message: string;
+};
+
+const VideoMessage = ({ message }: Props) => {
+  if (message) {
+    return (
+      <div className="call-info">
+        {" "}
+        <h1>{message}</h1>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+};
+
+export default VideoMessage;
