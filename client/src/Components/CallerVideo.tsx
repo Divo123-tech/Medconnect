22482faf447d180523c3f378087ae32196108009
Divo123 <topherharsh@gmail.com>
2025-03-31 14:12:56 +0700

    fixed typing issues in components

--- client/src/Components/CallerVideo.tsx
+++ client/src/Components/CallerVideo.tsx
@@ -1,95 +1,126 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useRef, useState } from "react";
-import './VideoPage.css'
-import { useSearchParams, useNavigate } from 'react-router-dom';
-import socketConnection from '../webrtcUtilities/socketConnection'
-import ActionButtons from './ActionButtons/ActionButtons'
+import "./VideoPage.css";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import socketConnection from "../webrtcUtilities/socketConnection";
+import ActionButtons from "./ActionButtons/ActionButtons";
 import VideoMessageBox from "./VideoMessageBox";
+type Props = {
+  remoteStream: any;
+  localStream: any;
+  peerConnection: any;
+  callStatus: any;
+  updateCallStatus: any;
+  offerData: any;
+  userName: any;
+};
+const CallerVideo = ({
+  remoteStream,
+  localStream,
+  peerConnection,
+  callStatus,
+  updateCallStatus,
+  userName,
+}: Props) => {
+  const remoteFeedEl = useRef(null); //this is a React ref to a dom element, so we can interact with it the React way
+  const localFeedEl = useRef(null); //this is a React ref to a dom element, so we can interact with it the React way
+  const navigate = useNavigate();
+  const [videoMessage, setVideoMessage] = useState(
+    "Please enable video to start!"
+  );
+  const [offerCreated, setOfferCreated] = useState(false);
 
-const CallerVideo = ({remoteStream, localStream,peerConnection,callStatus,updateCallStatus,userName})=>{
-    const remoteFeedEl = useRef(null); //this is a React ref to a dom element, so we can interact with it the React way
-    const localFeedEl = useRef(null); //this is a React ref to a dom element, so we can interact with it the React way
-    const navigate = useNavigate();
-    const [ videoMessage, setVideoMessage ] = useState("Please enable video to start!")
-    const [ offerCreated, setOfferCreated ] = useState(false)
+  //send back to home if no localStream
+  useEffect(() => {
+    if (!localStream) {
+      navigate(`/`);
+    } else {
+      //set video tags
+      remoteFeedEl.current.srcObject = remoteStream;
+      localFeedEl.current.srcObject = localStream;
+    }
+  }, []);
 
-    //send back to home if no localStream
-    useEffect(()=>{
-        if(!localStream){
-            navigate(`/`)
-        }else{
-            //set video tags
-            remoteFeedEl.current.srcObject = remoteStream
-            localFeedEl.current.srcObject = localStream            
+  //set video tags
+  // useEffect(()=>{
+  //     remoteFeedEl.current.srcObject = remoteStream
+  //     localFeedEl.current.srcObject = localStream
+  // },[])
+
+  //if we have tracks, disable the video message
+  useEffect(() => {
+    if (peerConnection) {
+      peerConnection.ontrack = (e) => {
+        if (e?.streams?.length) {
+          setVideoMessage("");
+        } else {
+          setVideoMessage("Disconnected...");
         }
-    },[])
-    
-    //set video tags
-    // useEffect(()=>{
-    //     remoteFeedEl.current.srcObject = remoteStream
-    //     localFeedEl.current.srcObject = localStream
-    // },[])
+      };
+    }
+  }, [peerConnection]);
 
-    //if we have tracks, disable the video message
-    useEffect(()=>{
-        if(peerConnection){
-            peerConnection.ontrack = e=>{
-                if(e?.streams?.length){
-                    setVideoMessage("")
-                }else{
-                    setVideoMessage("Disconnected...")
-                }
-            }
-        }
-    },[peerConnection])
+  //once the user has shared video, start WebRTC'ing :)
+  useEffect(() => {
+    const shareVideoAsync = async () => {
+      const offer = await peerConnection.createOffer();
+      peerConnection.setLocalDescription(offer);
+      //we can now start collecing ice candidates!
+      // we need to emit the offer to the server
+      const socket = socketConnection(userName);
+      socket.emit("newOffer", offer);
+      setOfferCreated(true); //so that our useEffect doesn't make an offer again
+      setVideoMessage("Awaiting answer..."); //update our videoMessage box
+      console.log(
+        "created offer, setLocalDesc, emitted offer, updated videoMessage"
+      );
+    };
+    if (!offerCreated && callStatus.videoEnabled) {
+      //CREATE AN OFFER!!
+      console.log("We have video and no offer... making offer");
+      shareVideoAsync();
+    }
+  }, [callStatus.videoEnabled, offerCreated]);
 
-    //once the user has shared video, start WebRTC'ing :)
-    useEffect(()=>{
-        const shareVideoAsync = async()=>{
-            const offer = await peerConnection.createOffer()
-            peerConnection.setLocalDescription(offer)
-            //we can now start collecing ice candidates!
-            // we need to emit the offer to the server
-            const socket = socketConnection(userName)
-            socket.emit('newOffer',offer)
-            setOfferCreated(true) //so that our useEffect doesn't make an offer again
-            setVideoMessage("Awaiting answer...") //update our videoMessage box
-            console.log("created offer, setLocalDesc, emitted offer, updated videoMessage")
-        }
-        if(!offerCreated && callStatus.videoEnabled){
-            //CREATE AN OFFER!!
-            console.log("We have video and no offer... making offer")
-            shareVideoAsync()
-        }
-    },[callStatus.videoEnabled,offerCreated])
-    
+  useEffect(() => {
+    const addAnswerAsync = async () => {
+      await peerConnection.setRemoteDescription(callStatus.answer);
+      console.log("Answer added!!");
+    };
+    if (callStatus.answer) {
+      addAnswerAsync();
+    }
+  }, [callStatus]);
 
-    useEffect(()=>{
-        const addAnswerAsync = async()=>{
-            await peerConnection.setRemoteDescription(callStatus.answer)
-            console.log("Answer added!!")
-        }
-        if(callStatus.answer){
-            addAnswerAsync()
-        }
-    },[callStatus])
+  return (
+    <div>
+      <div className="videos">
+        <VideoMessageBox message={videoMessage} />
+        <video
+          id="local-feed"
+          ref={localFeedEl}
+          autoPlay
+          controls
+          playsInline
+        ></video>
+        <video
+          id="remote-feed"
+          ref={remoteFeedEl}
+          autoPlay
+          controls
+          playsInline
+        ></video>
+      </div>
+      <ActionButtons
+        localFeedEl={localFeedEl}
+        remoteFeedEl={remoteFeedEl}
+        callStatus={callStatus}
+        localStream={localStream}
+        updateCallStatus={updateCallStatus}
+        peerConnection={peerConnection}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <div className="videos">
-                <VideoMessageBox message={videoMessage} />
-                <video id="local-feed" ref={localFeedEl} autoPlay controls playsInline></video>
-                <video id="remote-feed" ref={remoteFeedEl} autoPlay controls playsInline></video> 
-            </div>
-            <ActionButtons 
-                localFeedEl={localFeedEl}
-                remoteFeedEl={remoteFeedEl}
-                callStatus={callStatus}
-                localStream={localStream}
-                updateCallStatus={updateCallStatus}
-                peerConnection={peerConnection}
-            />
-        </div>
-    )
-}
-
-export default CallerVideo
+export default CallerVideo;
--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,132 +1,168 @@
-import { useEffect } from 'react'
-import prepForCall from '../webrtcUtilities/prepForCall'
-import socketConnection from '../webrtcUtilities/socketConnection'
-import clientSocketListeners from '../webrtcUtilities/clientSocketListeners'
-import { useState } from 'react'
-import createPeerConnection from '../webrtcUtilities/createPeerConn'
-import { useSearchParams, useNavigate } from 'react-router-dom';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react";
+import prepForCall from "../webrtcUtilities/prepForCall";
+import socketConnection from "../webrtcUtilities/socketConnection";
+import clientSocketListeners from "../webrtcUtilities/clientSocketListeners";
+import { useState } from "react";
+import createPeerConnection from "../webrtcUtilities/createPeerConn";
+import { useNavigate } from "react-router-dom";
+type Props = {
+  remoteStream: any;
+  localStream: any;
+  peerConnection: any;
+  setPeerConnection: any;
+  callStatus: any;
+  updateCallStatus: any;
+  offerData: any;
+  setOfferData: any;
+  userName: any;
+  setUserName: any;
+  setLocalStream: any;
+  setRemoteStream: any;
+};
+const Home = ({
+  callStatus,
+  updateCallStatus,
+  setLocalStream,
+  setRemoteStream,
+  remoteStream,
+  peerConnection,
+  setPeerConnection,
+  userName,
+  setUserName,
+  setOfferData,
+}: Props) => {
+  const [typeOfCall, setTypeOfCall] = useState();
+  const [joined, setJoined] = useState(false);
+  const [availableCalls, setAvailableCalls] = useState([]);
+  const navigate = useNavigate();
 
-const Home = ({callStatus,updateCallStatus,setLocalStream,
-    setRemoteStream,remoteStream,peerConnection,setPeerConnection,
-    localStream,userName, setUserName,offerData,setOfferData})=>{
+  //called on "Call" or "Answer"
+  const initCall = async (typeOfCall: any) => {
+    // set localStream and GUM
+    await prepForCall(callStatus, updateCallStatus, setLocalStream);
+    // console.log("gum access granted!")
+    setTypeOfCall(typeOfCall); //offer or answer
+  };
 
-    const [ typeOfCall, setTypeOfCall ] = useState()
-    const [joined, setJoined] = useState(false)
-    const [availableCalls, setAvailableCalls] = useState([])
-    const navigate = useNavigate();
+  //Test backend connection
+  // useEffect(()=>{
+  //     const test = async()=>{
+  //         const socket = socketConnection("test")
+  //     }
+  //     test()
+  // },[])
 
-    //called on "Call" or "Answer"
-    const initCall = async(typeOfCall)=>{
-        // set localStream and GUM
-        await prepForCall(callStatus,updateCallStatus,setLocalStream)
-        // console.log("gum access granted!")
-        setTypeOfCall(typeOfCall) //offer or answer
+  //Nothing happens until the user clicks join
+  //(Helps with React double render)
+  useEffect(() => {
+    if (joined) {
+      const userName = prompt("Enter username");
+      setUserName(userName);
+      const setCalls = (data: any) => {
+        setAvailableCalls(data);
+        console.log(data);
+      };
+      const socket = socketConnection(userName);
+      socket.on("availableOffers", setCalls);
+      socket.on("newOfferWaiting", setCalls);
     }
+  }, [joined, setUserName]);
 
-    //Test backend connection
-    // useEffect(()=>{
-    //     const test = async()=>{
-    //         const socket = socketConnection("test")
-    //     }
-    //     test()
-    // },[])
-    
-    //Nothing happens until the user clicks join
-    //(Helps with React double render)
-    useEffect(()=>{
-        if(joined){
-            const userName = prompt("Enter username")
-            setUserName(userName)
-            const setCalls = data=>{
-                setAvailableCalls(data)
-                console.log(data)
-            }
-            const socket = socketConnection(userName)
-            socket.on('availableOffers',setCalls)
-            socket.on('newOfferWaiting',setCalls)
-        }
-    },[joined])
-
-
-    //We have media via GUM. setup the peerConnection w/listeners
-    useEffect(()=>{
-        if(callStatus.haveMedia && !peerConnection){
-            // prepForCall has finished running and updated callStatus
-            const { peerConnection, remoteStream } = createPeerConnection(userName,typeOfCall)
-            setPeerConnection(peerConnection)
-            setRemoteStream(remoteStream)
-        }
-    },[callStatus.haveMedia])
-
-    //We know which type of client this is and have PC.
-    //Add socketlisteners
-    useEffect(()=>{
-        if(typeOfCall && peerConnection){
-            const socket = socketConnection(userName)
-            clientSocketListeners(socket,typeOfCall,callStatus,
-                updateCallStatus,peerConnection)
-        }
-    },[typeOfCall,peerConnection])
-
-    //once remoteStream AND pc are ready, navigate
-    useEffect(()=>{
-        if(remoteStream && peerConnection){
-            navigate(`/${typeOfCall}?token=${Math.random()}`)
-        }
-    },[remoteStream,peerConnection])
-
-    
-    const call = async()=>{
-        //call related stuff goes here
-        initCall('offer')
+  //We have media via GUM. setup the peerConnection w/listeners
+  useEffect(() => {
+    if (callStatus.haveMedia && !peerConnection) {
+      // prepForCall has finished running and updated callStatus
+      const { peerConnection, remoteStream } = createPeerConnection(
+        userName,
+        typeOfCall
+      );
+      setPeerConnection(peerConnection);
+      setRemoteStream(remoteStream);
     }
+  }, [
+    callStatus.haveMedia,
+    peerConnection,
+    setPeerConnection,
+    setRemoteStream,
+    typeOfCall,
+    userName,
+  ]);
 
-    const answer = (callData)=>{
-        //answer related stuff goes here
-        initCall('answer')
-        setOfferData(callData)
+  //We know which type of client this is and have PC.
+  //Add socketlisteners
+  useEffect(() => {
+    if (typeOfCall && peerConnection) {
+      const socket = socketConnection(userName);
+      clientSocketListeners(
+        socket,
+        typeOfCall,
+        callStatus,
+        updateCallStatus,
+        peerConnection
+      );
     }
+  }, [typeOfCall, peerConnection, userName, callStatus, updateCallStatus]);
 
-    if(!joined){
-        return(
-            <div className="container d-flex align-items-center justify-content-center min-vh-100">
-                <button 
-                    onClick={()=>setJoined(true)} 
-                    className="btn btn-primary btn-lg"
-                >Join</button>
-            </div> 
-        )
+  //once remoteStream AND pc are ready, navigate
+  useEffect(() => {
+    if (remoteStream && peerConnection) {
+      navigate(`/${typeOfCall}?token=${Math.random()}`);
     }
+  }, [remoteStream, peerConnection, navigate, typeOfCall]);
 
+  const call = async () => {
+    //call related stuff goes here
+    initCall("offer");
+  };
+
+  const answer = (callData: any) => {
+    //answer related stuff goes here
+    initCall("answer");
+    setOfferData(callData);
+  };
+
+  if (!joined) {
     return (
-        <div className="container">
-            <div className="row">
-                <h1>{userName}</h1>
-                <div className="col-6"> 
-                    <h2>Make a call</h2>
-                    <button 
-                        onClick={call} 
-                        className="btn btn-success btn-lg hang-up"
-                    >
-                        Start Call
-                    </button>
-                </div>
-                <div className="col-6"> 
-                    <h2>Available Calls</h2>
-                    {availableCalls.map((callData,i)=>
-                        <div className="col mb-2" key={i}>
-                            <button 
-                                onClick={()=>{answer(callData)}}
-                                className="btn btn-lg btn-warning hang-up"
-                        >
-                            Answer Call From {callData.offererUserName}
-                            </button>
-                        </div>
-                    )}
-                </div>
-            </div>
-        </div>
-    )
-}
+      <div className="container d-flex align-items-center justify-content-center min-vh-100">
+        <button
+          onClick={() => setJoined(true)}
+          className="btn btn-primary btn-lg"
+        >
+          Join
+        </button>
+      </div>
+    );
+  }
 
-export default Home
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>{userName}</h1>
+        <div className="col-6">
+          <h2>Make a call</h2>
+          <button onClick={call} className="btn btn-success btn-lg hang-up">
+            Start Call
+          </button>
+        </div>
+        <div className="col-6">
+          <h2>Available Calls</h2>
+          {availableCalls.map((callData: any, i) => (
+            <div className="col mb-2" key={i}>
+              <button
+                onClick={() => {
+                  answer(callData);
+                }}
+                className="btn btn-lg btn-warning hang-up"
+              >
+                Answer Call From {callData.offererUserName}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
