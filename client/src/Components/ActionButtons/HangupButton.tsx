5d38f31c937256e1f1847ca6e5da6bb891a24bb9
Divo123 <topherharsh@gmail.com>
2025-04-01 15:52:45 +0700

    made hangup reroute

--- client/src/Components/ActionButtons/HangupButton.tsx
+++ client/src/Components/ActionButtons/HangupButton.tsx
@@ -1,35 +1,46 @@
+import { Link, useNavigate } from "react-router";
+const HangupButton = ({
+  remoteFeedEl,
+  localFeedEl,
+  peerConnection,
+  callStatus,
+  setCallStatus,
+}) => {
+  const navigate = useNavigate();
+  const hangupCall = () => {
+    console.log("Before Func");
+    if (peerConnection) {
+      console.log("begining of func");
+      const copyCallStatus = { ...callStatus };
+      copyCallStatus.current = "complete";
+      //user has clicked hang up. pc:
+      //close it
+      //remove listeners
+      //set it to null
+      peerConnection.close();
+      peerConnection.onicecandidate = null;
+      peerConnection.onaddstream = null;
+      peerConnection = null;
 
-const HangupButton = ({remoteFeedEl, localFeedEl,peerConnection, callStatus, setCallStatus})=>{
-
-    const hangupCall = ()=>{
-        if(peerConnection){
-            const copyCallStatus = {...callStatus}
-            copyCallStatus.current = 'complete'
-            //user has clicked hang up. pc:
-                //close it
-                //remove listeners
-                //set it to null
-                peerConnection.close();
-                peerConnection.onicecandidate = null
-                peerConnection.onaddstream = null
-                peerConnection = null;
-
-            //set both video tags to empty
-            localFeedEl.current.srcObject = null;
-            remoteFeedEl.current.srcObject = null;
-        }
+      //set both video tags to empty
+      localFeedEl.current.srcObject = null;
+      remoteFeedEl.current.srcObject = null;
+      console.log("end of func");
     }
+    console.log("after func");
+  };
 
-    if(callStatus.current === "complete"){
-        return <></>
-    }
+  if (callStatus.current === "complete") {
+    return <></>;
+  }
 
-    return(
-        <button 
-            onClick={hangupCall} 
-            className="btn btn-danger hang-up"
-        >Hang Up</button>
-    )
-}
+  return (
+    <Link to="/">
+      <button onClick={hangupCall} className="btn btn-danger hang-up">
+        Hang Up
+      </button>
+    </Link>
+  );
+};
 
-export default HangupButton
+export default HangupButton;
