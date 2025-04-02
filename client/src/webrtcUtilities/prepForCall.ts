04ebd8a775ade56c42a9c71492eb2be6c1c3e1e6
Divo123 <topherharsh@gmail.com>
2025-04-02 14:59:29 +0700

    cleaned up messy Promise syntax in prepForCall

--- client/src/webrtcUtilities/prepForCall.ts
+++ client/src/webrtcUtilities/prepForCall.ts
@@ -1,32 +1,27 @@
 //Share this function for both sides, answer and caller
 // because both sides need to do this same thing before
 // we can move forward
-import socketConnection from "./socketConnection";
 
-const prepForCall = (callStatus, updateCallStatus, setLocalStream) => {
-  return new Promise<void>(async (resolve, reject) => {
-    //can bring constraints in as a param
-    const constraints = {
-      video: true, //must have one constraint, dont have to show it yet
-      audio: false,
-    };
+const prepForCall = async (callStatus, updateCallStatus, setLocalStream) => {
+  //can bring constraints in as a param
+  const constraints = {
+    video: true, //must have one constraint, dont have to show it yet
+    audio: false,
+  };
 
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      //update bools
-      const copyCallStatus = { ...callStatus };
-      copyCallStatus.haveMedia = true; //signals to the app that we have media
-      copyCallStatus.videoEnabled = null; //init both to false, you can init to true
-      copyCallStatus.audioEnabled = false;
-      updateCallStatus(copyCallStatus);
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    //update bools
+    const copyCallStatus = { ...callStatus };
+    copyCallStatus.haveMedia = true; //signals to the app that we have media
+    copyCallStatus.videoEnabled = null; //init both to false, you can init to true
+    copyCallStatus.audioEnabled = false;
+    updateCallStatus(copyCallStatus);
 
-      setLocalStream(stream);
-      resolve();
-    } catch (err) {
-      console.log(err);
-      reject(err);
-    }
-  });
+    setLocalStream(stream);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default prepForCall;
