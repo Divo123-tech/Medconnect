df70c759e30b6f9d18f047abb9ea9f5d4ab07b3f
Divo123 <topherharsh@gmail.com>
2025-04-19 15:55:38 +0800

    added User entity

--- server/src/main/java/com/backend/server/entities/User.java
+++ server/src/main/java/com/backend/server/entities/User.java
@@ -1,4 +1,33 @@
 package com.backend.server.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    public enum Role {
+        PATIENT,
+        DOCTOR
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
