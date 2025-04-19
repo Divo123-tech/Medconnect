baee7e4aa5417d113d47bc958dafb2d98f9d6a6c
Divo123 <topherharsh@gmail.com>
2025-04-19 23:49:24 +0800

    added JwtService

---
+++ server/src/main/java/com/backend/server/services/JwtService.java
@@ -0,0 +1,11 @@
+package com.backend.server.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUserName(String jwt) {
+        return "";
+    }
+}
