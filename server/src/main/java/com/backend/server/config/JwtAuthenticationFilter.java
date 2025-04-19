c8ecf3aaab624676fb152877444b442085e31be1
Divo123 <topherharsh@gmail.com>
2025-04-19 23:45:34 +0800

    added header and token extraction logic

--- server/src/main/java/com/backend/server/config/JwtAuthenticationFilter.java
+++ server/src/main/java/com/backend/server/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.backend.server.config;
 
+import com.backend.server.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +15,7 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(
@@ -21,6 +23,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain)
             throws ServletException, IOException {
+        //get the bearer token from the request header
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
 
+        //if the auth header is not or doesn't start with bearer
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            //pass the request and response along
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //get the jwt token from 7 which is removing Bearer and space
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
     }
 }
