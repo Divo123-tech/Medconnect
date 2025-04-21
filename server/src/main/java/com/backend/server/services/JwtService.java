77201a1faf59ba1b6f23cc6d22589130c4a9cf32
Divo123 <topherharsh@gmail.com>
2025-04-21 11:49:41 +0800

    added extractAllClaims

--- server/src/main/java/com/backend/server/services/JwtService.java
+++ server/src/main/java/com/backend/server/services/JwtService.java
@@ -1,11 +1,34 @@
 package com.backend.server.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
-    public String extractUserName(String jwt) {
+    private static final String SECRET_KEY = "AbnKH5JNyL3LGqinUzHWnUmvYZzCcbsxyBxDvrtOJPlm+W8B+ymyAiLiXzC257LB\n";
+
+    public String extractUserName(String token) {
         return "";
     }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
