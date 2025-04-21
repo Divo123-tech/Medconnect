a37a0eff25de5dd2f494c852e0956dc73675cfd6
Divo123 <topherharsh@gmail.com>
2025-04-21 12:41:14 +0800

    added UserDetails Service

--- server/src/main/java/com/backend/server/config/JwtAuthenticationFilter.java
+++ server/src/main/java/com/backend/server/config/JwtAuthenticationFilter.java
@@ -5,8 +5,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +20,11 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService, UserDetailsService userDetailsService1) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService1;
+    }
 
     @Override
     protected void doFilterInternal(
@@ -28,7 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String jwt;
         final String userEmail;
 
-        //if the auth header is not or doesn't start with bearer
+        //if the auth header is empty or doesn't start with bearer
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
             //pass the request and response along
             filterChain.doFilter(request, response);
@@ -38,5 +47,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         //get the jwt token from 7 which is removing Bearer and space
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUserName(jwt);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
     }
 }
--- server/src/main/java/com/backend/server/services/JwtService.java
+++ server/src/main/java/com/backend/server/services/JwtService.java
@@ -2,11 +2,18 @@ package com.backend.server.services;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,9 +21,49 @@ public class JwtService {
     private static final String SECRET_KEY = "AbnKH5JNyL3LGqinUzHWnUmvYZzCcbsxyBxDvrtOJPlm+W8B+ymyAiLiXzC257LB\n";
 
     public String extractUserName(String token) {
-        return "";
+        return extractClaim(token, Claims::getSubject);
     }
 
+
+    public<T> T extractClaim(String token, Function<Claims, T>claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUserName(token);
+        //checks if the token is valid by checking whether the username in the token
+        //is the same one as the userDetails
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        //checks whether the token expiration has passed
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims, UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                //makes the whole thing a string
+                .compact() ;
+    }
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
