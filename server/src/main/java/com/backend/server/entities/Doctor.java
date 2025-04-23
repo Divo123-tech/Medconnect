832d47257e9ac85d3912108b87c0ff763c0f980f
Divo123 <topherharsh@gmail.com>
2025-04-23 13:05:21 +0800

    Added Doctor entity

---
+++ server/src/main/java/com/backend/server/entities/Doctor.java
@@ -0,0 +1,33 @@
+package com.backend.server.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "doctors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Doctor extends User {
+
+    private String specialization;
+
+    @Column(name = "started_practicing_at")
+    private LocalDate startedPracticingAt;
+
+    private String education;
+
+    private String bio;
+
+}
