0a6266a93e5fc701d8f7a75ae9bfb2e5323809d6
Divo123 <topherharsh@gmail.com>
2025-04-23 12:52:53 +0800

    Added Patient entity to extend user and add unique columns

---
+++ server/src/main/java/com/backend/server/entities/Patient.java
@@ -0,0 +1,28 @@
+package com.backend.server.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "patients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Patient extends User {
+
+    private String phoneNumber;
+    private Double height;
+    private Double weight;
+
+    @Enumerated(EnumType.STRING)
+    private BloodType bloodType;
+
+    private String conditions;
+
+    public enum BloodType {
+        A_POS, A_NEG, B_POS, B_NEG, AB_POS, AB_NEG, O_POS, O_NEG
+    }
+}
