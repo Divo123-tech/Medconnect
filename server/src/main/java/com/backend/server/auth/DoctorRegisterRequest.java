80fa7e26eaf21b5aa03aaa8a361e01d425726cb0
Divo123 <topherharsh@gmail.com>
2025-04-23 13:25:21 +0800

    Added DoctorRegisterRequest

---
+++ server/src/main/java/com/backend/server/auth/DoctorRegisterRequest.java
@@ -0,0 +1,24 @@
+package com.backend.server.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorRegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String specialization;
+    private LocalDate startedPracticingAt;
+    private String education;
+    private String bio;
+}
+
---
+++ server/src/main/java/com/backend/server/services/DoctorService.java
@@ -0,0 +1,38 @@
+package com.backend.server.services;
+
+import com.backend.server.auth.AuthenticationResponse;
+import com.backend.server.entities.Patient;
+import com.backend.server.entities.User;
+import com.backend.server.repositories.PatientRepository;
+import com.backend.server.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class DoctorService {
+    private final UserRepository userRepository;
+    private final PatientRepository patientRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+//    public AuthenticationResponse registerDoctor(RegisterRequest request) {
+//        //assume that if they're using the basic register route they're not an admin
+//        //and only patients can be registered in this route
+//        Patient patient = Patient.builder()
+//                .firstName(request.getFirstName())
+//                .lastName(request.getLastName())
+//                .email(request.getEmail())
+//                .password(passwordEncoder.encode(request.getPassword()))
+//                .role(User.Role.PATIENT)
+//                .build();
+//
+//        patientRepository.save(patient);
+//        var jwtToken = jwtService.generateToken(patient);
+//        return AuthenticationResponse.builder().token(jwtToken).build();
+//    }
+}
