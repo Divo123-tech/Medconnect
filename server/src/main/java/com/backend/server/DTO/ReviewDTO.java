8f9178bb9ffb68fdb4459078d681b97d11fb9a1d
Divo123 <topherharsh@gmail.com>
2025-05-22 14:37:57 +0800

    Added ReviewDTO.java

---
+++ server/src/main/java/com/backend/server/DTO/ReviewDTO.java
@@ -0,0 +1,23 @@
+package com.backend.server.DTO;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReviewDTO {
+
+    private Long id;
+
+    private Integer rating;         // 1 to 5
+    private String title;
+    private String body;
+    private LocalDateTime createdAt;
+    private String patientFirstName;
+    private String patientLastName;
+    private String patientEmail;
+    private String patientProfilePicture;
+}
