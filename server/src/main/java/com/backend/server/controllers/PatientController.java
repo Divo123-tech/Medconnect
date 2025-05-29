a45008fd2dd3f1d14c333041888282a62dcf2d22
Divo123 <topherharsh@gmail.com>
2025-05-29 14:54:02 +0800

    made PatientController more concise

--- server/src/main/java/com/backend/server/controllers/PatientController.java
+++ server/src/main/java/com/backend/server/controllers/PatientController.java
@@ -1,5 +1,6 @@
 package com.backend.server.controllers;
 
+import com.backend.server.DTO.UserDTO;
 import com.backend.server.entities.Patient;
 import com.backend.server.entities.User;
 import com.backend.server.services.PatientService;
@@ -7,6 +8,7 @@ import com.backend.server.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,22 +19,12 @@ import static com.backend.server.DTO.ToDTOMaps.mapToPatientDTO;
 @RequestMapping("/api/v1/patients")
 @RequiredArgsConstructor
 public class PatientController {
-
     private final PatientService patientService;
 
-    private final UserService userService;
-
     @GetMapping("/{id}")
-    public ResponseEntity<?> getPatientById(@PathVariable int id, Authentication auth) {
-        User currentUser = userService.getUserById(id);
-        User userRequesting = (User) auth.getPrincipal(); // this is safe
-        // Ensure only doctors can access patient profiles
-        if (userRequesting.getRole() != User.Role.DOCTOR && userRequesting.getRole() != User.Role.ADMIN) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied. Only doctors can view patient profiles.");
-        }
-
+    @PreAuthorize("hasRole('DOCTOR')")
+    public ResponseEntity<UserDTO.PatientGetProfileDTO> getPatientById(@PathVariable int id) {
         Patient patient = patientService.getPatientById(id);
-
         return ResponseEntity.ok(mapToPatientDTO(patient));
     }
 }
